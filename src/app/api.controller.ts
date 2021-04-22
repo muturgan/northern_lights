f01f61f8dd41f5fe3aa3106622347162ea79c46e
Andrey Sakharov <sakharov.a@n-t.io>
2021-04-22 22:55:12 +0300

    base routing was added

---
+++ src/app/api.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, UseFilters, UsePipes, UseInterceptors, Post, HttpCode, HttpStatus } from '@nestjs/common';
+
+
+// @UsePipes()
+// @UseFilters()
+// @UseInterceptors()
+
+
+@Controller('api')
+export class ApiController
+{
+   @Post('registration')
+   @HttpCode(HttpStatus.OK)
+   public async register(): Promise<void>
+   {
+   }
+
+
+   @Post('check')
+   @HttpCode(HttpStatus.OK)
+   public async check(): Promise<void>
+   {
+   }
+
+
+   @Post('activate')
+   @HttpCode(HttpStatus.OK)
+   public async activate(): Promise<void>
+   {
+   }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { AppModule } from './app/app.module';
+
+async function bootstrap(): Promise<NestFastifyApplication>
+{
+   const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+   );
+
+   await app.listen('7777', '0.0.0.0')
+      .then(() => console.info(`App running on ${'port'}`));
+
+   return app;
+}
+export default bootstrap();
