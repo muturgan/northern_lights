f3fc5d718e7594a1d586b6a9fafff712561ebb13
Andrey Sakharov <sakharov.a@n-t.io>
2021-04-24 19:59:32 +0300

    promocode toLocaleLowerCase

--- src/promo/dto/promo.dto.ts
+++ src/promo/dto/promo.dto.ts
@@ -1,6 +1,8 @@
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class PromoDto {
+    @Transform(({ value }) => value.toLocaleLowerCase())
     @IsString()
     @IsNotEmpty()
     public readonly promocode!: string;
