6cf8de7821a180a02afcef9533aa794573c55c89
Andrey Sakharov <sakharov.a@n-t.io>
2021-04-26 00:54:03 +0300

    registration page was added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module, Type } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import path from 'path';
 
 import { configOptions } from './config';
 import { Promo, User } from './promo/dal';
@@ -11,19 +12,28 @@ const dbModels = [
    User,
 ];
 
-@Module({
-   imports: [
-      PromoModule,
-      ConfigModule.forRoot(configOptions),
-      TypeOrmModule.forRootAsync({
-         imports: [ConfigModule],
-         useFactory: (config: ConfigService) => {
-            const dbConfig = config.get('DB_CONFIG');
-            dbConfig.entities = dbModels;
-            return dbConfig;
-         },
-         inject: [ConfigService],
-     }),
-   ],
-})
+const imports: Array<DynamicModule | Type<any>> = [
+   PromoModule,
+   ConfigModule.forRoot(configOptions),
+   TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => {
+         const dbConfig = config.get('DB_CONFIG');
+         dbConfig.entities = dbModels;
+         return dbConfig;
+      },
+      inject: [ConfigService],
+   }),
+];
+
+if (process.env.NODE_ENV === 'dev') {
+   const NestStatic = require('@nestjs/serve-static');
+   imports.push(
+      NestStatic.ServeStaticModule.forRoot({
+         rootPath: path.join(process.cwd(), 'static'),
+      }),
+   );
+}
+
+@Module({imports})
 export class AppModule {}
