89dae5cc2a3812a45e427910dd8d9526592588e3
Andrey Sakharov <sakharov.a@n-t.io>
2021-05-04 01:59:19 +0300

    webp

--- gzip.js
+++ gzip.js
@@ -3,6 +3,7 @@ const path = require('path');
 const cp = require('child_process');
 
 const EXTENSIONS = ['.html', '.css', '.js'];
+const IMG_EXTENSIONS = ['.png', '.jpg', '.jpeg'];
 const STATIC_ROOT = path.join(process.cwd(), 'static');
 
 compressFolder(STATIC_ROOT);
@@ -15,6 +16,11 @@ function compressFolder(rootPath) {
       const entryPath = path.join(rootPath, entry);
       if (EXTENSIONS.some((ext) => entry.endsWith(ext))) {
          compressFile(entryPath);
+      }
+      else if (IMG_EXTENSIONS.some((ext) => entry.endsWith(ext))) {
+         entry.endsWith(IMG_EXTENSIONS[0])
+            ? pngToWebp(entryPath)
+            : jpgToWebp(entryPath);
       }
       else {
          const extName = path.extname(entry);
@@ -30,4 +36,12 @@ function compressFolder(rootPath) {
 
 function compressFile(filePath) {
    cp.exec(`gzip -9 -c ${filePath} > ${filePath}.gz`);
+}
+
+function jpgToWebp(filePath) {
+   cp.exec(`cwebp -af -m 6 -mt -q 100 ${filePath} -o ${filePath}.webp -quiet`);
+}
+
+function pngToWebp(filePath) {
+   cp.exec(`cwebp -af -m 6 -mt -lossless -near_lossless 95 ${filePath} -o ${filePath}.webp -quiet`);
 }
