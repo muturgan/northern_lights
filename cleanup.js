54c8fdf144dc5321cfa4b09f905bbc8290697e5b
Andrey Sakharov <sakharov.a@n-t.io>
2021-05-10 22:06:43 +0300

    do not cleanup gziped files

--- cleanup.js
+++ cleanup.js
@@ -2,15 +2,18 @@ const path = require('path');
 const fs = require('fs');
 
 const TRASH_EXPORT = 'export {};';
+const JS_EXT = '.js';
 
 const rootPath = path.join(process.cwd(), 'static', 'js', 'generated');
 const files = fs.readdirSync(rootPath);
 
 files.forEach((fileName) => {
-    const filePath = path.join(rootPath, fileName);
-    const content = fs.readFileSync(filePath).toString('utf8');
-    const cleaned = content.replace(TRASH_EXPORT, '');
-    fs.writeFileSync(filePath, cleaned);
-    console.log(`${fileName} cleaned`);
+    if (fileName.endsWith(JS_EXT)) {
+        const filePath = path.join(rootPath, fileName);
+        const content = fs.readFileSync(filePath).toString('utf8');
+        const cleaned = content.replace(TRASH_EXPORT, '');
+        fs.writeFileSync(filePath, cleaned);
+        console.log(`${fileName} cleaned`);
+    }
 });
 
