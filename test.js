c0038ff4f90ceb6e61c98ee55c5644ca8ce001c3
Andrey Sakharov <sakharov.a@n-t.io>
2021-05-26 21:26:56 +0300

    it does not matters

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 const fileBuf = fs.readFileSync('./bip39_russian.txt');
 const content = fileBuf.toString('utf8');
 const words = content.split('\n');
-const longWords = words.filter(w => w.length <4);
+const longWords = words.filter(w => w.length > 7);
 console.log(longWords);
